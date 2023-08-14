aab7aa303d6a67d3d08ece27b3ce045d201796b8
milkawinoh <milkawinoh@gmail.com>
2023-08-14 05:13:19 -0700

    task 1

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+a function that prints all integers of a list.
+"""
+
+
+def print_list_integer(my_list=[]):
+
+    for list in my_list:
+        print("{}".format(list))
