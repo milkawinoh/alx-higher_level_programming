631aa51c5d124daa05f9378a60f1418898bb9aa1
milkawinoh <milkawinoh@gmail.com>
2023-08-14 09:10:33 -0700

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+a function that prints a matrix of integers.
+"""
+
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for n in row:
+            print("{:d}".format(n), end="")
+        print()
