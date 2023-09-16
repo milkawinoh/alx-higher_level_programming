1f84db50694d1efabd7d36ba83d8cd0077b60838
milkawinoh <milkawinoh>
2023-09-16 06:02:50 +0300

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,28 @@
+class Rectangle:
+    """no imported module"""
+    def __init__(self,width=0,height=0):
+        self.__width = width
+        self.__heigth = height
+        
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self,value):
+        if not isinstance(self.__width,int):
+            raise TypeError("width must be an integer")
+        if self.__width < 0:
+            raise ValueError("Width must be >= 0")
+        
+    @property
+    def height(self):
+        return self.__heigth
+    
+    @height.setter
+    def height(self,value):
+        if not isinstance(self.__heigth,int):
+            raise TypeError("height must be an integer")
+        if self.__heigth < 0:
+            raise ValueError("height must be >= 0")
+
