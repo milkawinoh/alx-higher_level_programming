7e92f0de2de0e5f5af5fbae3f8e91b593d3595cc
milkawinoh <milkawinoh>
2023-09-22 16:57:48 +0300

    task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,23 +8,28 @@ provides methods for JSON serialization and deserialization.
 
 import json
 
+
 class Base:
     """
     Base Class
 
     Attributes:
         id (int): The unique identifier for the object.
-    
+
     Class Attributes:
-        __nb_objects (int): A class attribute to keep track of the number of objects created.
+        __nb_objects (int): A class attribute to keep
+          track of the number of objects created.
 
     Methods:
         __init__(self, id=None): Initializes a new Base instance.
         nb_objects (property): Gets the number of objects created.
-        nb_objects (setter): Sets the number of objects created (read-only).
-        to_json_string(list_dictionaries): Converts a list of dictionaries to a JSON string.
+        nb_objects (setter): Sets the number of objects created
+        (read-only).
+        to_json_string(list_dictionaries): Converts a list of
+        dictionaries to a JSON string.
         save_to_file(cls, list_objs): Saves a list of objects to a JSON file.
-        from_json_string(json_string): Loads a list of objects from a JSON string.
+        from_json_string(json_string): Loads a list of objects from
+          a JSON string.
     """
 
     __nb_objects = 0
@@ -98,7 +103,8 @@ class Base:
         Loads a list of objects from a JSON string.
 
         Args:
-            json_string (str): A JSON string representation of a list of dictionaries.
+            json_string (str): A JSON string representation of
+            a list of dictionaries.
 
         Returns:
             list: A list of objects loaded from the JSON string.
@@ -106,3 +112,48 @@ class Base:
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Creates an instance with all attributes set based on a dictionary.
+
+        Args:
+            **dictionary: A dictionary containing attribute-value pairs.
+
+        Returns:
+            Base: An instance with attributes set based on the dictionary.
+        """
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy_instance = cls(1)
+
+        # Use the update method to assign attributes based on the dictionary.
+        dummy_instance.update(**dictionary)
+
+        return dummy_instance
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        Returns a list of instances based on data from a JSON file.
+
+        Returns:
+            list: A list of instances loaded from the JSON file.
+        """
+        filename = cls.__name__ + ".json"
+
+        try:
+            with open(filename, mode='r', encoding='utf-8') as file:
+                json_data = file.read()
+
+            # Deserialize the JSON data into a list of dictionaries.
+            data_list = cls.from_json_string(json_data)
+
+            # Create instances from the list of dictionaries.
+            instances = [cls.create(**data) for data in data_list]
+
+            return instances
+        except FileNotFoundError:
+            return []
